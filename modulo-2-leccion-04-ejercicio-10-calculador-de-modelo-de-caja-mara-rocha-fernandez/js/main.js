374d59f88a3917b26702d8c92eb8738f154117ab
Mara Rocha Fernández <maranhadisenha@gmail.com>
2021-12-06 14:38:05 +0100

    add exercise 10 - lesson 4 - module 2

---
+++ modulo-2-leccion-04-ejercicio-10-calculador-de-modelo-de-caja-mara-rocha-fernandez/js/main.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// función para calcular anchos según modelo de caja
+function boxModelCalculator(borderBox, width, padding, borderWidth) {
+    // dos formas de calcular ancho del contenido y ancho total de la caja
+
+    // versión 1: condicional clásico
+    /*
+    let contentWidth = width;
+    let totalWidth = width;
+    if (borderBox) {
+        // modelo de caja tipo border-box
+        contentWidth = width - padding * 2 - borderWidth * 2;
+    } else {
+        // modelo de caja tipo content-box
+        totalWidth = width + padding * 2 + borderWidth * 2;
+    }
+    */
+
+    // versión 2: condicional ternario
+    let contentWidth = borderBox ? (width - padding * 2 - borderWidth * 2) : width;
+    let totalWidth = borderBox ? width : (width + padding * 2 + borderWidth * 2);
+
+    // devolver resultado en cadena de texto
+    return `El ancho del contenido es: ${contentWidth} y el ancho total de la caja es: ${totalWidth}.`;
+}
+
+// caja a: tipo border-box, ancho 50px, padding 10px y borde 2px
+const boxA = boxModelCalculator(true, 50, 10, 2);
+console.log(boxA);
+
+// caja b: tipo content-box, ancho 100px, padding 5px y borde 5px
+const boxB = boxModelCalculator(false, 100, 5, 5);
+console.log(boxB);
+
+/***/
+
+// función anterior transformada en función arrow
+const boxModelCalculatorArrowFunction = (borderBox, width, padding, borderWidth) => {
+    let contentWidth = borderBox ? (width - padding * 2 - borderWidth * 2) : width;
+    let totalWidth = borderBox ? width : (width + padding * 2 + borderWidth * 2);
+    return `El ancho del contenido es: ${contentWidth} y el ancho total de la caja es: ${totalWidth}.`;
+}
+
+// caja a: tipo border-box, ancho 50px, padding 10px y borde 2px
+console.log(boxModelCalculatorArrowFunction(true, 50, 10, 2));
+
+// caja b: tipo content-box, ancho 100px, padding 5px y borde 5px
+console.log(boxModelCalculatorArrowFunction(false, 100, 5, 5));
